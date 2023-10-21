4d938582a68163bcaa0f9099fd16bae1f64df78c
“Jiaqi_Jin” <fdsam@hotmail.com>
2023-10-21 21:27:46 +0100

    update

--- lib/Page/event_editing_page.dart
+++ lib/Page/event_editing_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sampleproject/Utils/utils.dart';
 import 'package:sampleproject/model/event.dart';
 
 class EventEditingPage extends StatefulWidget {
@@ -12,6 +13,8 @@ class EventEditingPage extends StatefulWidget {
 }
 
 class _EventEditingPageState extends State<EventEditingPage> {
+  final _formKey = GlobalKey<FormState>();
+  final titleController = TextEditingController();
   late DateTime fromDate;
   late DateTime toDate;
 
@@ -26,12 +29,29 @@ class _EventEditingPageState extends State<EventEditingPage> {
   }
 
   @override
+  void dispose() {
+    titleController.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          leading: CloseButton(),
-          actions: buildEditingAction(),
+      appBar: AppBar(
+        leading: CloseButton(),
+        actions: buildEditingAction(),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(12),
+        child: Form(
+          key: _formKey,
+          child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+            buildTitle(),
+            SizedBox(height: 12),
+            buildDatTimePickers(),
+          ]),
         ),
-      );
+      ));
 
   List<Widget> buildEditingAction() => [
         ElevatedButton.icon(
@@ -42,4 +62,59 @@ class _EventEditingPageState extends State<EventEditingPage> {
             icon: Icon(Icons.done),
             label: Text('SAVE'))
       ];
+
+  Widget buildTitle() => TextFormField(
+        style: TextStyle(fontSize: 24),
+        decoration: InputDecoration(
+          border: UnderlineInputBorder(),
+          hintText: 'Add Title',
+        ),
+        onFieldSubmitted: (_) {},
+        validator: (title) =>
+            title != null && title.isEmpty ? 'Title cannot be empty' : null,
+        controller: titleController,
+      );
+
+  Widget buildDatTimePickers() => Column(
+        children: [
+          buildFrom(),
+        ],
+      );
+
+  Widget buildFrom() => Buildheader(
+    header : 'FROM',
+    child : Row(
+        children: [
+          Expanded(
+            flex: 2, // more space
+            child: buildDropdownField(
+                text: Utils.toDate(fromDate), onClicked: () {}),
+          ),
+          Expanded(
+              child: buildDropdownField(
+                  text: Utils.toTime(fromDate), onClicked: () {})),
+        ],
+      )
+  );
+
+  Widget buildDropdownField({
+    required String text,
+    required VoidCallback onClicked,
+  }) =>
+      ListTile(
+        title: Text(text),
+        trailing: Icon(Icons.arrow_drop_down),
+        onTap: onClicked,
+      );
+
+  Widget Buildheader({
+    required String header,
+    required Widget child,
+  }) => 
+  Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(header, style: TextStyle(fontWeight: FontWeight.bold),)
+    ],
+  )
 }
